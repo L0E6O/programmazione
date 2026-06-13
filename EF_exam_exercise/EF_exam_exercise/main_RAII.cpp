7a144be31a8a9a561c8ba05b2b2f74fd0711cd84
Leonardo Iarossi <leonardo.iarossi@icloud.com>
2026-06-13 20:02:42 +0200

    finito il main del RAII

--- EF_exam_exercise/EF_exam_exercise/main_RAII.cpp
+++ EF_exam_exercise/EF_exam_exercise/main_RAII.cpp
@@ -1,13 +1,17 @@
+#include <iostream>
+#include <memory>
+#include <mutex>
 #include <stdexcept>
 
 class HardwareDevice {
 public:
     HardwareDevice(int intr = 0) : interrupt(intr) {}
 
-    void lowLevelOp() throw(std::runtime_error) { // ... something
+    void lowLevelOp() throw(std::runtime_error) {
         if (!interrupt) {
-            // FIXME launch exception
+            throw std::runtime_error("Interrupt is equal to 0");
         }
+        std::cout << "Low level op successfull" << std::endl;
     }
     // other methods...
 private:
@@ -30,15 +34,39 @@ private:
     HardwareDevice* const dvc;
 };
 
+class DeviceManagerRAII {
+public:
+    DeviceManagerRAII(int interrupt) : hd(new HardwareDevice(interrupt)) {}
+
+    void use() {
+        hd->lowLevelOp();
+    }
+
+private:
+    std::unique_ptr<HardwareDevice> const hd = std::make_unique<HardwareDevice>();
+};
+
 void test(int interrupt) {
     DeviceManager* mgr = new DeviceManager(interrupt);
-    // XXX it may throw an exception
-    mgr->use();
-    // ...
+
+    try {
+        mgr->use();
+    } catch (std::runtime_error e) {
+        delete mgr;
+        throw std::runtime_error(e.what());
+    }
+
     delete mgr;
+}
+
+void testNotRaw(int interrupt) {
+    auto mgr = std::make_unique<DeviceManagerRAII>(interrupt);
+
+    mgr->use(); //throws std::runtime_error
 }
 
 int main() {
     test(3);
     test(0); // will throw
+    testNotRaw(2);
 }
